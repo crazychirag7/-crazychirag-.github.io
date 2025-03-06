b0ef2312109dd0235306a8725d9f10691dee060c
crazychirag7 <chiragmegasync@gmail.com>
2025-03-06 14:09:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script>
+
 let movies = [];
 
 document.getElementById('movieForm').addEventListener('submit', function (e) {
